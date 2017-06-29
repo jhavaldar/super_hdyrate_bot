925d143c2ac1509e31877b6006d3c5245d399d8c
jhavaldar <havaldar@sas.upenn.edu>
2017-06-29 16:59:59 -0400

    half a second update

--- hydrate.py
+++ hydrate.py
@@ -5,9 +5,9 @@ import tweepy, time, sys, csv, math, requests, random, os
 from datetime import timedelta, datetime
 from bs4 import BeautifulSoup
 
-frequency = 3
-start = "16:56"
-end = "16:59"
+frequency = 30
+start = "17:00"
+end = "17:30"
 
 # Get auth object
 def get_auth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET):
